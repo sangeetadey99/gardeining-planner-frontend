ab29b702f5c12c79f1f8b9adf766984d28c154cd
sangeetadey99 <sdey6268@gmail.com>
2026-02-28 22:39:47 +0530

    added render backend url

--- gardening-frontend-client/src/services/api.js
+++ gardening-frontend-client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://gardeining-planner-backend-server.onrender.com/api",
 });
 
 API.interceptors.request.use((req) => {
